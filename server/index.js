4b04aa2be3c1651031b8f6d308b0c2ba8bdc0a4c
Justin <just.inchong4life@gmail.com>
2023-01-04 15:45:13 -0800

    about to restructure files

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ const controllers = require('./controllers');
 const app = express();
 const PORT = process.env.PORT;
 
+
+app.get('/', (req, res) => {
+  res.send('qa service');
+});
+
 app.use(express.json());
 app.use(cors());
 app.use(express.static('../client/dist'));
